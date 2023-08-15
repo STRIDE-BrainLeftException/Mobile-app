8477feb85d52bf5ef312bb758b155904fe8f9977
eshans <eshans2000@gmail.com>
2023-08-16 03:27:13 +0530

    finish bottom tab

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,4 +3,4 @@ import { Dimensions } from "react-native";
 export const WIDTH = Dimensions.get("window").width;
 export const HEIGHT = Dimensions.get("window").height;
 
-export const BOTTOM_TAB_BAR_HEIGHT = 100;
+export const BOTTOM_TAB_BAR_HEIGHT = 70;
