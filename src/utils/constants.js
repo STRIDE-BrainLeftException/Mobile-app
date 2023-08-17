d83b6fdd74d0fdf458b36578262345fadb607ca4
eshans <eshans2000@gmail.com>
2023-08-17 12:26:07 +0530

    add planet animations

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,4 +5,8 @@ export const HEIGHT = Dimensions.get("window").height;
 
 export const BOTTOM_TAB_BAR_HEIGHT = 70;
 
-export const ONBOARDING_BOTTOM_COLOR = "#05014a"
+export const ONBOARDING_BOTTOM_COLOR = "#05014a";
+
+export const BORDER_RADIUS = 20;
+export const BLURVIEW_BORDER_WIDTH = 1;
+export const BLUEVIEW_BORDER_COLOR = "rgba(255,255,255,0.2)";
