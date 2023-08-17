2915eb71bc43488a1220e9e1fccab4eb78a76a20
Radith Samarakoon <jjeli1234@gmail.com>
2023-08-17 23:30:36 +0530

    almost done

--- src/components/ShipViewComponent.js
+++ src/components/ShipViewComponent.js
@@ -5,13 +5,154 @@ import {
   ImageBackground,
   StatusBar,
   Text,
+  Image,
+  TouchableOpacity,
 } from "react-native";
-import BlurViewCard from "../components/basic/BlurViewCard";
+import Carousel, { Pagination } from "react-native-snap-carousel";
+import { BlurView } from "expo-blur";
 
-const ShipViewComponent = () => {
+const ShipViewComponent = ({ shipData, handleShipSelection }) => {
+  console.log(shipData.image);
+
+  const shipTypeStyle =
+    shipData.type === "HyperStride" ? styles.shipType[0] : styles.shipType[1];
+
   return (
-    <BlurViewCard>
-      <Text>{`\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n`}</Text>
-    </BlurViewCard>
+    <View style={styles.root}>
+      <Image source={shipData.image} style={styles.shipImage} />
+      <View style={styles.shipHeader}>
+        <Text style={shipTypeStyle}>{shipData.type}</Text>
+        <View style={styles.shipTitleContainer}>
+          <TouchableOpacity onPress={handleShipSelection.previous}>
+            <Text style={styles.shipSelectButtons}>{`<`}</Text>
+          </TouchableOpacity>
+          <Text style={styles.shipTitle}>{shipData.title}</Text>
+          <TouchableOpacity onPress={handleShipSelection.next}>
+            <Text style={styles.shipSelectButtons}>{`>`}</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={styles.blurViewContainer}>
+        <BlurView style={styles.blurView} tint="dark" intensity={30}>
+          <Text style={styles.shipEngine}>{shipData.engine}</Text>
+          <Text style={styles.shipDescription}>{shipData.description}</Text>
+          <View style={styles.shipStatsContainer}>
+            <View style={styles.shipStatContainer}>
+              <Text style={styles.shipStat}>{shipData.price}</Text>
+              <Text style={styles.shipStatFooter}>Price</Text>
+            </View>
+            <View style={styles.shipStatContainer}>
+              <Text style={styles.shipStat}>{shipData.passengers}</Text>
+              <Text style={styles.shipStatFooter}>Passengers</Text>
+            </View>
+            <View style={styles.shipStatContainer}>
+              <Text style={styles.shipStat}>{shipData.arrivalTime}</Text>
+              <Text style={styles.shipStatFooter}>Expected Arrival Time</Text>
+            </View>
+          </View>
+          <Carousel />
+          <Text>{`\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n`}</Text>
+          <Text>{`\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n`}</Text>
+        </BlurView>
+      </View>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  shipHeader: {
+    zIndex: 2,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    position: "absolute",
+    top: "85%",
+  },
+  shipType: [
+    {
+      color: "#3dc5ff",
+      borderWidth: 1,
+      borderRadius: 15,
+      padding: 5,
+      borderColor: "#3dc5ff",
+    },
+    {
+      color: "#3eff3c",
+      borderWidth: 1,
+      borderRadius: 15,
+      padding: 5,
+      borderColor: "#3eff3c",
+    },
+  ],
+  shipStatsContainer: {
+    display: "flex",
+    flexDirection: "row",
+    gap: 20,
+    marginTop: 15,
+  },
+  shipStatContainer: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  shipStat: {
+    color: "#1dbbff",
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+  shipStatFooter: {
+    color: "#1dbbff",
+    fontSize: 8,
+  },
+  shipEngine: {
+    color: "white",
+    fontSize: 15,
+  },
+  shipDescription: {
+    color: "#b9eaff",
+    fontSize: 10,
+    marginTop: 3,
+  },
+  shipTitleContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  shipImage: {
+    position: "relative",
+    zIndex: 1,
+  },
+  shipSelectButtons: {
+    paddingHorizontal: 20,
+    fontSize: 30,
+    color: "white",
+  },
+  shipTitle: {
+    fontSize: 40,
+    color: "white",
+    fontWeight: "bold",
+  },
+  blurView: {
+    alignItems: "center",
+    paddingTop: "20%",
+  },
+  blurViewContainer: {
+    color: "white",
+    paddingTop: "23%",
+    paddingHorizontal: "6%",
+    position: "absolute",
+    top: "65%",
+    width: "100%",
+    borderRadius: 20,
+    overflow: "hidden",
+    backgroundColor: "rgba(0,0,0,0.1)",
+    alignSelf: "center",
+  },
+});
+
+export default ShipViewComponent;
--- src/screens/ShipSelectionScreen.js
+++ src/screens/ShipSelectionScreen.js
@@ -1,38 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet, ImageBackground, StatusBar } from "react-native";
 import ShipViewComponent from "../components/ShipViewComponent";
 
 const shipData = [
   {
     title: "Star Dust C90",
+    type: "HyperStride",
+    engine: "Hyper Thrusters | 34LYpK",
     description:
       "Lorem  ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    price: "899N",
+    price: "899Ñ",
     passengers: "200",
-    arrivalTime: "3",
-    image: require("../assets/images/Booking_Process/Ship_Selection_Screen_1/Star-dust-ship-img"),
+    arrivalTime: "3 days",
+    image: require("../assets/images/Booking_Process/Ship_Selection_Screen_1/Star-dust-ship-img.png"),
     visualArchive: [
-      "../assets/images/Booking_Process/Ship_Selection_Screen_1/img-1.png",
-      "../assets/images/Booking_Process/Ship_Selection_Screen_1/img-2.png",
-      "../assets/images/Booking_Process/Ship_Selection_Screen_1/img-3.png",
+      require("../assets/images/Booking_Process/Ship_Selection_Screen_1/img-1.png"),
+      require("../assets/images/Booking_Process/Ship_Selection_Screen_1/img-2.png"),
+      require("../assets/images/Booking_Process/Ship_Selection_Screen_1/img-3.png"),
     ],
   },
   {
     title: "Exploriz 69",
+    type: "Hib 2.0",
+    engine: "Hi-Nova Thrusters | 16LYpK",
     description:
       "Lorem  ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    price: "899N",
+    price: "648Ñ",
+    passengers: "600",
+    arrivalTime: "761 days",
+    image: require("../assets/images/Booking_Process/Ship_Selection_Screen_2/exploeriz-ship-img.png"),
+    visualArchive: [
+      require("../assets/images/Booking_Process/Ship_Selection_Screen_2/img-1.png"),
+      require("../assets/images/Booking_Process/Ship_Selection_Screen_2/img-2.png"),
+      require("../assets/images/Booking_Process/Ship_Selection_Screen_2/img-3.png"),
+    ],
   },
 ];
 
 const ShipSelectionScreen = () => {
+  const [selectedShipID, setSelectedShipID] = useState(0);
+
+  const handleShipSelection = {
+    next: () => {
+      if (selectedShipID < shipData.length - 1) {
+        setSelectedShipID(selectedShipID + 1);
+      } else {
+        setSelectedShipID(0);
+      }
+    },
+
+    previous: () => {
+      if (selectedShipID > 0) {
+        setSelectedShipID(selectedShipID - 1);
+      } else {
+        setSelectedShipID(shipData.length - 1);
+      }
+    },
+  };
+
   return (
     <ImageBackground
       source={require("../assets/images/Booking_BG.png")}
       style={styles.backgroundImage}
     >
       <StatusBar />
-      <ShipViewComponent />
+      <ShipViewComponent
+        shipData={shipData[selectedShipID]}
+        handleShipSelection={handleShipSelection}
+      />
     </ImageBackground>
   );
 };
