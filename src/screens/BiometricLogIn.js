917faf7a54da64a39a66145c202bbc0f1769b459
Radith Samarakoon <jjeli1234@gmail.com>
2023-08-19 16:53:29 +0530

    minor fix

--- src/screens/BiometricLogIn.js
+++ src/screens/BiometricLogIn.js
@@ -41,7 +41,7 @@ const styles = StyleSheet.create({
     fontSize: 25,
     color: "#fff",
   },
-  logo: { backgroundColor: "red", alignItems: "center", top: 450 },
+  logo: { backgroundColor: "transparent", alignItems: "center", top: 450 },
   image: { width: "100%", height: "100%" },
   cardHead: { color: "#fff", fontSize: 25 },
   cardText: {
